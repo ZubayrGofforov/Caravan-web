d8dca1b8746a4881762a3091c3ac3823848a0709
jumab <jumab@HP>
2023-01-25 10:03:46 +0500

    detached

--- Caravan.Web/Controllers/AccountsController.cs
+++ Caravan.Web/Controllers/AccountsController.cs
@@ -1,18 +1,75 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Caravan.Service.Common.Exceptions;
+using Caravan.Service.Dtos.Accounts;
+using Caravan.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Caravan.Web.Controllers;
 [Route("accounts")]
 public class AccountsController : Controller
 {
+    private readonly IAccountService _service;
+    public AccountsController(IAccountService accountService)
+    {
+        this._service = accountService;
+    }
+
+
     [HttpGet("register")]
     public ViewResult Register()
     {
         return View("Register");
     }
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync(AccountRegisterDto accountRegisterDto)
+    {
+        if (ModelState.IsValid)
+        {
+            bool result = await _service.RegisterAsync(accountRegisterDto);
+            if (result)
+            {
+                return RedirectToAction("login", "accounts", new { area = "" });
+            }
+            else
+            {
+                return Register();
+            }
+        }
+        else return Register();
+    }
+
 
     [HttpGet("login")]
     public ViewResult Login()
     {
         return View("Login");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LoginAsync(AccountLoginDto accountLoginDto)
+    {
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                string token = await _service.LoginAsync(accountLoginDto);
+                HttpContext.Response.Cookies.Append("X-Access-Token", token, new CookieOptions()
+                {
+                    HttpOnly = true,
+                    SameSite = SameSiteMode.Strict
+                });
+                return RedirectToAction("Index", "Home", new { area = "" });
+            }
+            catch (ModelErrorException modelError)
+            {
+                ModelState.AddModelError(modelError.Property, modelError.Message);
+                return Login();
+            }
+            catch
+            {
+                return Login();
+            }
+        }
+        else return Login();
     }
 }
---
+++ src/Caravan.Service/Common/Exceptions/ModelErrorException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caravan.Service.Common.Exceptions
+{
+    public class ModelErrorException : Exception
+    {
+        public string Property { get; set; } = String.Empty;
+
+        public ModelErrorException(string property, string message)
+            : base(message)
+        {
+            this.Property = property;
+        }
+    }
+}
