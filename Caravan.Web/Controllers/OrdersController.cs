cba0e3e4fd25591bd86ef33a71f6894e73155ac2
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-01-29 17:32:55 +0500

    Add Controller

--- Caravan.Web/Controllers/OrdersController.cs
+++ Caravan.Web/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 ﻿using Caravan.Service.Common.Utils;
+using Caravan.Service.Dtos.Orders;
 using Caravan.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,5 +18,33 @@ public class OrdersController : Controller
     {
         var orders = await _orderService.GetAllAsync(new PaginationParams(page, _pageSize));
         return View("Index", orders);
+    }
+
+    [HttpGet("orderId")]
+    public async Task<ViewResult> GetAsync(long orderId)
+    {
+        var product = await _orderService.GetAsync(orderId);
+        return View(product);
+    }
+
+    [HttpGet("Create")]
+    public ViewResult Create()
+    {
+        return View("OrderCreate");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(OrderCreateDto createDto)
+    {
+        if (ModelState.IsValid)
+        {
+            var res = await _orderService.CreateAsync(createDto);
+            if (res)
+            {
+                return RedirectToAction("Index", "Orders", new { area = "" });
+            }
+            else return Create();
+        }
+        else return Create();
     }
 }
