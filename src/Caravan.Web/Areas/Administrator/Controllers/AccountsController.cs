f43e9613f1fef2376d2c7212b470bc0e73a8cf00
jumab <jumab@HP>
2023-01-30 16:53:47 +0500

    Admin panel register & login create

---
+++ src/Caravan.Web/Areas/Administrator/Controllers/AccountsController.cs
@@ -0,0 +1,28 @@
+﻿using Caravan.Service.Dtos.Admins;
+using Caravan.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Caravan.Web.Areas.Administrator.Controllers;
+[Route("adminAccounts")]
+public class AccountsController : BaseController
+{
+    private readonly IAccountService _accountService;
+
+    public AccountsController(IAccountService accountService)
+    {
+        this._accountService = accountService;
+    }
+
+    [HttpGet("adminRegister")]
+    public ViewResult Register()
+    {
+        return View("AdminRegister");
+    }
+
+
+    [HttpGet("adminLogin")]
+    public ViewResult Login()
+    {
+        return View("AdminLogin");
+    }
+}
