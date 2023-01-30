b85bcdb4543c7cdb6dcfa92640583bc26c7f0e51
Fazliddin <Fazliddinmustafoyev3101@gmail.com>
2023-01-30 16:48:11 +0500

    AdminService

---
+++ src/Caravan.Service/Services/AdminService.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Caravan.DataAccess.Interfaces.Common;
+using Caravan.Service.Common.Exceptions;
+using Caravan.Service.Common.Utils;
+using Caravan.Service.Dtos.Admins;
+using Caravan.Service.Interfaces;
+using Caravan.Service.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Caravan.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Caravan.DataAccess.Repositories.Common;
+
+namespace Caravan.Service.Services
+{
+    public class AdminService : IAdminService
+    {
+        private readonly IUnitOfWork _repository;
+        private readonly IMapper _mapper;
+        private readonly IImageService _imageService;
+        private readonly IPaginatorService _paginatorService;
+
+        public AdminService(IUnitOfWork unitOfWork, IMapper mapper, IImageService imageService, IPaginatorService paginatorService)
+        {
+            _repository = unitOfWork;
+            _mapper = mapper;
+            _imageService = imageService;
+            _paginatorService = paginatorService;
+        }
+        public async Task<bool> DeleteAsync(long id)
+        {
+            var admin = _repository.Administrators.FirstOrDefaultAsync(x => x.Id == id);
+            if (admin is null)
+            {
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Admin not found"); 
+            }
+            _repository.Administrators.Delete(id);
+            var res = await _repository.SaveChangesAsync();
+            return res > 0;
+        }
+
+        public async Task<IEnumerable<AdminViewModel>> GetAllAsync(PaginationParams @params)
+        {
+            var query = _repository.Administrators.GetAll().OrderBy(x => x.CreatedAt).ToList()
+                .ConvertAll(x => _mapper.Map<AdminViewModel>(x));
+            var data = await _paginatorService.ToPagedAsync(query, @params.PageNumber, @params.PageSize);
+            return data;
+        }
+
+        public async Task<AdminViewModel> GetByIdAsync(long id)
+        {
+            var admin = await _repository.Administrators.FindByIdAsync(id);
+            if (admin is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Admin is not found");
+
+            var res = _mapper.Map<AdminViewModel>(admin);
+            return res;
+        }
+
+        public async Task<bool> UpdateAsync(long id, AdminCreateDto dto)
+        {
+            var admin = await _repository.Administrators.FindByIdAsync(id);
+            if (admin is null)
+                throw new StatusCodeException(HttpStatusCode.NotFound, "Admin is not found");
+            var res = _mapper.Map<Administrator>(admin);
+             _repository.Administrators.TrackingDeteched(res);
+            _repository.Administrators.Update(id,_mapper.Map<Administrator>(dto));
+            var result = await _repository.SaveChangesAsync();
+            return result > 0;
+        }
+    }
+}
