db79bab60e8ddd7c1e015956b623b27b7efea0a4
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-02-01 16:58:51 +0500

    Update layout

--- src/Caravan.Web/Areas/Administrator/Controllers/OrdersController.cs
+++ src/Caravan.Web/Areas/Administrator/Controllers/OrdersController.cs
@@ -4,7 +4,7 @@ using Caravan.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Caravan.Web.Areas.Administrator.Controllers;
-[Route("orders")]
+[Route("adminorders")]
 public class OrdersController : BaseController
 {
     private readonly IOrderService _orderService;
@@ -13,6 +13,18 @@ public class OrdersController : BaseController
     public OrdersController(IOrderService orderService)
     {
         this._orderService = orderService;
+    }
+    public async Task<ViewResult> Index(int page = 1)
+    {
+        var orders = await _orderService.GetAllAsync(new PaginationParams(page, _pageSize));
+        return View(orders);
+    }
+
+    [HttpGet("orderId")]
+    public async Task<ViewResult> GetAsync(long orderId)
+    {
+        var order = await _orderService.GetAsync(orderId);
+        return View(order);
     }
 
     [HttpGet("create")]
@@ -32,16 +44,4 @@ public class OrdersController : BaseController
         else return Create();
     }
 
-    public async Task<ViewResult> Index(int page = 1)
-    {
-        var orders = await _orderService.GetAllAsync(new PaginationParams(page, _pageSize));
-        return View(orders);
-    }
-
-    [HttpGet("orderId")]
-    public async Task<ViewResult> GetAsync(long orderId)
-    {
-        var order = await _orderService.GetAsync(orderId);
-        return View(order);
-    }
 }
