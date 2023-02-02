0057d05fb13bafa3acb91524e09084f62e6d2988
jumab <jumab@HP>
2023-02-02 14:51:16 +0500

    Updated

--- src/Caravan.Web/Areas/Administrator/Controllers/BaseController.cs
+++ src/Caravan.Web/Areas/Administrator/Controllers/BaseController.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace Caravan.Web.Areas.Administrator.Controllers;
-[Area("administrator")]
+[Area("administrator"), Authorize(Roles = "Admin")]
 public class BaseController : Controller
 {
    
