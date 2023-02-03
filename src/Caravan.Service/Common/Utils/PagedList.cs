04e2dcbb26b9f2c61975a613cecb2b4f59ab111c
Qahhor <nurmamatovnodirbek43@gmail.com>
2023-02-03 15:57:57 +0500

    awd

--- src/Caravan.Service/Common/Utils/PagedList.cs
+++ src/Caravan.Service/Common/Utils/PagedList.cs
@@ -1,39 +1,22 @@
-﻿//namespace Caravan.Service.Common.Utils;
-//public class PagedList<T> :List<T>
-//{
-//    public PaginationMetaData MetaData { get; set; } = default!;
+﻿using Microsoft.EntityFrameworkCore;
 
-//    public PagedList(PaginationParams @params, int totalItems, List<T> items)
-//    {
-//        this.MetaData = new PaginationMetaData(totalItems, @params.PageNumber, @params.PageSize);
-//        AddRange(items);
+namespace Caravan.Service.Common.Utils;
+public class PagedList<T> : List<T>
+{
+    public PaginationMetaData MetaData { get; set; } = default!;
 
-<<<<<<< HEAD
-//    }
+    public PagedList(List<T> items,PaginationParams @params, int totalItems)
+    {
+        this.MetaData = new PaginationMetaData(@params.PageNumber, @params.PageSize, totalItems);
+        AddRange(items);
 
-//    public async Task<PagedList<T>> ToPagedListAsync(List<T> source, PaginationParams @params)
-//    {
-//        int totalItems = source.Count();
-//        var result =  source.Skip((@params.PageNumber-1)*@params.PageSize)
-//            .Take(@params.PageSize);
-//        return new PagedList<T>(@params, totalItems, result.ToList());
-//    }
-//}
-=======
-        public PagedList(List<T> items,PaginationParams @params, int totalItems)
-        {
-            this.MetaData = new PaginationMetaData(@params.PageNumber, @params.PageSize, totalItems);
-            AddRange(items);
-
-        }
-
-        public static async Task<PagedList<T>> ToPagedListAsync(IQueryable<T> source, PaginationParams @params)
-        {
-            int totalItems = source.Count();
-            var result = await source.Skip((@params.PageNumber-1)*@params.PageSize)
-                .Take(@params.PageSize).ToListAsync();
-            return new PagedList<T>(result ,@params, totalItems);
-        }
     }
-}
->>>>>>> 5c1bc51 (Add Pagination)
+
+    public static async Task<PagedList<T>> ToPagedListAsync(IQueryable<T> source, PaginationParams @params)
+    {
+        int totalItems = source.Count();
+        var result = await source.Skip((@params.PageNumber-1)*@params.PageSize)
+            .Take(@params.PageSize).ToListAsync();
+        return new PagedList<T>(result ,@params, totalItems);
+    }
+ }
