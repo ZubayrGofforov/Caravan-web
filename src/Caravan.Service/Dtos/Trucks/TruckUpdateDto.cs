bf57df45e8271ba5c1c8a7c07aba9a10de8282bf
NormadjonGofforov <jumab@HP>
2023-02-20 22:31:40 +0500

    Updated

--- src/Caravan.Service/Dtos/Trucks/TruckUpdateDto.cs
+++ src/Caravan.Service/Dtos/Trucks/TruckUpdateDto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Caravan.Service.Common.Attributes;
+using Microsoft.AspNetCore.Http;
 using System.ComponentModel.DataAnnotations;
 
 namespace Caravan.Service.Dtos.Trucks
@@ -6,14 +7,24 @@ namespace Caravan.Service.Dtos.Trucks
     public class TruckUpdateDto
     {
         public long Id { get; set; }
+
+        [Required]
+        [MaxLength(30), MinLength(3)]
         public string Name { get; set; } = string.Empty;
+
+        [MaxFileSize(2)]
+        [AllowedFiles(new string[] { ".jpg", ".png", ".jpeg", ".svg", ".webp" })]
         public IFormFile? Image { get; set; }
+
+        [Required]
+        [CheckNumber]
         public double? MaxLoad { get; set; }
 
         public string? LocationName { get; set; }
 
         public string? Description { get; set; }
 
+        [Required]
         public string TruckNumber { get; set; } = string.Empty;
     }
 }
